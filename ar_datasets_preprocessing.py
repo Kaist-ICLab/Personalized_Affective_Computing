e9b6d1d6ed68c24d172f165e97ea592727668751
yjhan99 <yjhan99@kaist.ac.kr>
2024-01-31 15:24:03 +0000

    finalize

--- ar_datasets_preprocessing.py
+++ ar_datasets_preprocessing.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
     # dataset = Wesad(GLOBAL_LOGGER, config['Paths']['wesad_dir']).get_dataset()
     # dataset.save(config['Paths']['mts_out_dir'])
 
-    # dataset = KEmoWork(GLOBAL_LOGGER, config['Paths']['kemowork_dir'], 'STRESS').get_dataset()
-    # dataset.save(config['Paths']['mts_out_dir'])
-
     # dataset = Case(GLOBAL_LOGGER, config['Paths']['case_dir'], 'AROUSAL').get_dataset()
     # dataset.save(config['Paths']['mts_out_dir'])
 
-    dataset = KEmoCon(GLOBAL_LOGGER, config['Paths']['kemocon_dir'], 'arousal').get_dataset()
+    # dataset = KEmoCon(GLOBAL_LOGGER, config['Paths']['kemocon_dir'], 'arousal').get_dataset()
+    # dataset.save(config['Paths']['mts_out_dir'])
+
+    dataset = ASCERTAIN(GLOBAL_LOGGER, config['Paths']['ascertain_dir'], 'AROUSAL').get_dataset()
     dataset.save(config['Paths']['mts_out_dir'])
